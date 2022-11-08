34e9ef6412f065860aa395155d1529b17b61abe2
KaranDasari <117617697+KaranDasari@users.noreply.github.com>
2022-11-08 13:35:49 +0530

    Update commerceCloudDeploy.groovy

--- vars/commerceCloudDeploy.groovy
+++ vars/commerceCloudDeploy.groovy
@@ -2,7 +2,7 @@ def call(buildName, dbUpdateMode, environmentId, strategy) {
     echo "##### Initiate Deployment to SAP Commerce Cloud Environment #####"
     //deploy tag 
     script{
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'HarrisScarfeCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentialsHS', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
             deploy = sh (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"buildCode\": \"${buildName}\",\"databaseUpdateMode\": \"${dbUpdateMode}\",\"environmentCode\": \"${environmentId}\",\"strategy\": \"${strategy}\"}'",returnStdout:true)
             echo "$deploy"
             deploy_result = readJSON text: "$deploy"
