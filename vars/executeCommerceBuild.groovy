ac8b60320ff37a051f39864144ff275950638ca9
Vishnu <vishnu.burra@bestandless.com.au>
2023-09-18 14:27:19 +1000

    Update executeCommerceBuild.groovy

--- vars/executeCommerceBuild.groovy
+++ vars/executeCommerceBuild.groovy
@@ -3,7 +3,7 @@ def call(commerceDir) {
 	sh "cd ${commerceDir}/js-storefront/spartacus-ssr"
 	echo "##### Executing [yarn, install] #####"
 	sh "cd ${commerceDir}/js-storefront/spartacus-ssr&& npm install -g yarn && yarn install"
-	echo "##### Executing [yarn, build:ssr] #####"
+	echo "##### Executing [yarn, build:ssr-stg] #####"
 	sh "cd ${commerceDir}/js-storefront/spartacus-ssr && ng update --all && yarn run build:ssr-stg"
 	echo "##### Executing dist folder commit #####"
 	sh "cd ${commerceDir}/js-storefront/spartacus-ssr/dist && git add . && git commit -m 'Push from jenkins' && git push"
